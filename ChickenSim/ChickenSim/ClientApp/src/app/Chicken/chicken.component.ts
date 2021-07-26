083762bfdae8e6b96f6e91138c475c375232f569
james <owsingames@yahoo.com>
2021-07-26 16:11:11 -0400

    Created Chicken Component

---
+++ ChickenSim/ChickenSim/ClientApp/src/app/Chicken/chicken.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+  selector: 'app-chicken-menu',
+  templateUrl: './chicken.component.html',
+  //styleUrls: ['./product-menu.component.css'],
+  //providers: [ChickenSimSerivce]
+})
+
+export class ChickenComponent {
+  //allChickens: Chicken[] = [];
+}
