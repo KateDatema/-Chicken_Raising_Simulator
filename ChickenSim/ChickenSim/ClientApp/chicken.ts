bb3a87d3b66064aaf145edf75432d32199306c77
Kate Datema <k8datema@gmail.com>
2021-07-26 16:06:59 -0400

    added chicken and farm models, and created a service - need to test later as my build is not currently worked due to internet and hardware

---
+++ ChickenSim/ChickenSim/ClientApp/chicken.ts
@@ -0,0 +1,12 @@
+export interface Chicken {
+  favoriteID: number | null;
+  userID: string;
+  questionID: number;
+  id: number;
+  name: string;
+  age: number;
+  smarts: number;
+  strength: number;
+  luck: number;
+  color: string;
+}
